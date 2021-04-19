0a1dd6b6500e4f88ea870e9206bd8c9132cfaf0f
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2021-04-19 22:47:12 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,12 @@
 class Backer
   attr_accessor :name, :backed_projects
+
   def initialize (name)
     @name = name
     @backed_projects = []
   end
 
-  def backed_projects(project)
+  def back_project(project)
     @backed_projects<<project
   end
 end
